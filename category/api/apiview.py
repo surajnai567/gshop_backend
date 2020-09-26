bb33adc176d7c1600a27f4c9e3102dfcf62f459d
surajnai567 <surajnai567@gmail.com>
2020-09-26 22:51:18 +0530

    api

--- category/api/apiview.py
+++ category/api/apiview.py
@@ -3,6 +3,7 @@ from django.http.response import JsonResponse
 from category.models import CategoryModel
 from .serializer import CategorySerializer
 import json
+import staticdata
 
 
 class CategoryApiView(APIView):
@@ -10,6 +11,8 @@ class CategoryApiView(APIView):
 		token = json.loads(request.body.decode('utf-8'))
 		category = CategoryModel.objects.all()
 		serial_data = CategorySerializer(category, many=True).data
+		for i in serial_data:
+			i['cateimg'] = staticdata.CLOUDARY_BASE_URL + i['cateimg']
 		return JsonResponse({"code": 200, "status": "success", "categories": serial_data})
 
 
