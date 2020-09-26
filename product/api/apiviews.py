3ce18747d6992faadd2e2c8d32e11e22ad8d5cdb
surajnai567 <surajnai567@gmail.com>
2020-09-26 22:52:29 +0530

    api

--- product/api/apiviews.py
+++ product/api/apiviews.py
@@ -4,6 +4,7 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 import json
 from category.models import CategoryModel
+import staticdata
 
 
 class GetAllProductApiView(APIView):
@@ -19,6 +20,8 @@ class GetNewProductApiView(APIView):
 		try:
 			products = Product.objects.filter(is_new=True)
 			serialize_products = ProductSerializer(products, many=True).data
+			for i in serialize_products:
+				i['image'] = staticdata.CLOUDARY_BASE_URL+i['image']
 		except:
 			pass
 		return JsonResponse({"code": "200", "status": "success", "products": serialize_products})
@@ -32,6 +35,8 @@ class GetHomePageProductApiView(APIView):
 		try:
 			products = Product.objects.filter(homepage=True)
 			serialize_products = ProductSerializer(products, many=True).data
+			for i in serialize_products:
+				i['image'] = staticdata.CLOUDARY_BASE_URL + i['image']
 		except:
 			pass
 		return JsonResponse({"code": "200", "status": "success", "products": serialize_products})
@@ -44,10 +49,12 @@ class GetCategoryProductApiView(APIView):
 		cat = CategoryModel.objects.filter(categry=category['categry'])[0]
 		print(cat)
 		token = ''
-		serialize_products = ''
+		serialize_products = []
 		try:
 			products = Product.objects.filter(category=cat.id)
 			serialize_products = ProductSerializer(products, many=True).data
+			for i in serialize_products:
+				i['image'] = staticdata.CLOUDARY_BASE_URL + i['image']
 		except:
 			pass
 		return JsonResponse({"code": "200", "status": "success", "products": serialize_products})
@@ -62,6 +69,8 @@ class GetSearchProductApiView(APIView):
 		try:
 			products = Product.objects.all()
 			serialize_products = ProductSerializer(products, many=True).data
+			for i in serialize_products:
+				i['image'] = staticdata.CLOUDARY_BASE_URL + i['image']
 		except:
 			pass
 		return JsonResponse({"code": "200", "status": "success", "products": serialize_products})
